d5f9d6706711da9ad4b2fed3e58fcce245b537ff
Andrew <112577337+vremyavnikuda@users.noreply.github.com>
2022-11-10 19:11:04 +0700

    creating a while folder

---
+++ src/while_cycle/while0.java
@@ -0,0 +1,11 @@
+package while_cycle;
+
+public class while0 {
+    public static void main(String[]args){
+        int a =13;
+        int b =4;
+        a*=4;
+        b+=50;
+        System.out.println((++a>--b)?a-7:b*2);
+    }
+}

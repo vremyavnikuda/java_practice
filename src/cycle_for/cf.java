f013eedbd5738deb1d3fcecc17b89bfcac885bcf
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-15 08:56:20 +0700

    //task1 cycle for

--- src/cycle_for/cf.java
+++ src/cycle_for/cf.java
@@ -1,7 +1,19 @@
+/*
+Повторение 1
+Пользователь вводит с клавиатуры натуральное число n.
+Затем вводит n целых чисел. Необходимо найти и вывести на экран их сумму.
+
+Sample Input:
+3
+7
+2
+9
+
+Sample Output:
+18
+*/
 package cycle_for;
-
 import java.util.Scanner;
-
 public class cf {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);

ab11507238ac7791607d9d3b1598096bdbdde81f
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-15 09:28:28 +0700

    cycle for (task3)

---
+++ src/cycle_for/cf2.java
@@ -0,0 +1,25 @@
+/*
+Опять Сумма
+Вводится 2 целых числа a и b (a <= b).
+Необходимо вычислить сумму всех чисел от а до b включительно.
+
+Sample Input:
+1
+3
+
+Sample Output:
+6
+*/
+package cycle_for;
+import java.util.Scanner;
+public class cf2 {
+    public static void main (String[]args){
+        Scanner sc =new Scanner(System.in);
+        int a =sc.nextInt(),b=sc.nextInt();//вводим 2 целых числа
+        int ab=0;//сумма этих чисел
+        for (int i=a;i<=b;i++){
+            ab+=i;
+        }
+        System.out.print(ab);
+    }
+}

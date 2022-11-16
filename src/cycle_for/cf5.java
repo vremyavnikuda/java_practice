70a7ea9d80d51966c1b19f428be499a2a684d645
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-16 16:09:31 +0700

    upload сщтвÑcondition task

---
+++ src/cycle_for/cf5.java
@@ -0,0 +1,13 @@
+package cycle_for;
+
+import java.util.Scanner;
+
+public class cf5 {
+    public static void main (String[]args){
+        Scanner sc =new Scanner(System.in);
+        int q = sc.nextInt();
+        for (int i=0;i<q;i++){
+
+        }
+    }
+}

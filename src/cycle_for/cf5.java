a7be844ce65a9b8b963350ef1eda302535859e0a
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-16 16:44:12 +0700

    обновление условия и добавление комментариев к решению
    ++

--- src/cycle_for/cf5.java
+++ src/cycle_for/cf5.java
@@ -1,3 +1,21 @@
+/*Min и Max
+Задача на поиск минимума и максимума. Пользователь вводит натуральное число n, затем n целых чисел.
+Необходимо определить и вывести на экран минимальное и максимальное из них.
+Обратите внимание, что в большинстве задач как минимальное и максимальное значения часто используются -2*109 и 2*109 соответственно.
+Но в олимпиадных задачах всегда смотрите на ограничения по числам (в них всегда оговорены границы, в которых будут лежать значения переменных).
+
+Sample Input:
+5
+6
+10
+-7
+13
+4
+
+Sample Output:
+-7
+13
+*/
 package cycle_for;
 
 import java.util.Scanner;
@@ -5,9 +23,18 @@ import java.util.Scanner;
 public class cf5 {
     public static void main (String[]args){
         Scanner sc =new Scanner(System.in);
-        int q = sc.nextInt();
+        //переменная ввода данных
+        //вводим модификатор и тип MAX MIN
+        int q = sc.nextInt();int min = Integer.MAX_VALUE,max=Integer.MIN_VALUE;
         for (int i=0;i<q;i++){
-
+            int q1= sc.nextInt();//указываем условие ввода данных
+            //ищем максмимальное значение
+            if (q1>max)max=q1;
+            //ищем минимальное значение
+            if (q1<min)min=q1;
         }
+        //выводим результат
+        System.out.println(min);
+        System.out.println(max);
     }
 }

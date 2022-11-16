84e3cc8bf91fdb482bf12764d84a4d1f5b044668
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-16 15:59:24 +0700

    cycle for (break and continue)

---
+++ src/cycle_for/cf4.java
@@ -0,0 +1,39 @@
+package cycle_for;
+
+import java.util.Scanner;
+
+public class cf4 {
+    public static void main (String[]args){
+        /*Scanner sc = new Scanner(System.in);
+        String q =sc.next();
+        boolean is_q= false;
+        for (String i ="СТОП";!i.equals(q);q= sc.next()){
+            if (q=="Куб"){
+                is_q=true;
+            }
+        }
+        if(is_q==true) {
+            System.out.println("Yes");
+        }
+        else if (is_q==false) {
+            System.out.println("No");
+        }*/
+        Scanner sc =new Scanner(System.in);//модуль сканера
+        int q =0;
+        for (;true;){
+            String q1= sc.nextLine();//зацикливаем бесконечный ввод данных
+            if (q1.equals("Куб")){//если будет введен "Куб" ,то меняем значение переменной q==1
+                q=1;//q==1
+            }
+            if (q1.equals("СТОП")){//Если введенно значение "СТОП" и q==1 ,то выводим значение "YES"
+                if (1==q){//условие вывода "YES"
+                    System.out.println("YES");//Вывод "YES"
+                }
+                else{//иначе
+                    System.out.println("NO");//"NO"
+                }
+                break;//оставнавливаем цикл
+            }
+        }
+    }
+}

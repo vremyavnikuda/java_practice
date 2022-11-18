fc9e3b24b2c9224778f57f269ef1a6a4b762c3b8
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-18 16:51:53 +0700

    cycle task 6 level A "нечетные цифры"

---
+++ src/cycle_for_A/cf_A_4.java
@@ -0,0 +1,15 @@
+package cycle_for_A;
+
+import java.util.Scanner;
+
+public class cf_A_4 {
+    public static void main (String[]args){
+        //исходное значение (диапозон)
+        int q=10;
+        //условие (если i==1 и i<10,тогда к i+2
+        for (int i=1;i<10;i+=2){
+            //вывести результат
+            System.out.println(i+" ");
+        }
+    }
+}

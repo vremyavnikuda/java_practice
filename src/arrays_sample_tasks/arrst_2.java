f267822adbe5cd646862facd80c9d60ddf4e1673
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-24 03:39:31 +0700

    upd condition

--- src/arrays_sample_tasks/arrst_2.java
+++ src/arrays_sample_tasks/arrst_2.java
@@ -1,7 +1,25 @@
+/*
+Задача 2
+Вводится натуральное число n.
+Допишите код, который создаст массив с именем array на n элементов,
+в котором буду храниться квадраты чисел от 0 до n - 1 (элемент равен квадрату индекса).
+
+Написанный код менять запрещено.
+
+Sample Input 1:
+5
+
+Sample Output 1:
+0 1 4 9 16
+
+Sample Input 2:
+3
+
+Sample Output 2:
+0 1 4
+*/
 package arrays_sample_tasks;
-
 import java.util.Scanner;
-
 public class arrst_2 {
     public static void main (String []args){
         Scanner sc=new Scanner(System.in);

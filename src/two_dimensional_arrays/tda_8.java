1cb9792fcba9ebee08409746e02357301409a250
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-27 02:07:14 +0700

    upd task tda_8

--- src/two_dimensional_arrays/tda_8.java
+++ src/two_dimensional_arrays/tda_8.java
@@ -1,7 +1,24 @@
+/*
+Поворот на 90
+Вводятся 2 натуральных числа: n и m.
+Затем вводится двумерный массив размером n*m.
+Поверните его на 90 градусов по часовой стрелке и выведите на экран (через пробел).
+
+Sample Input:
+3
+4
+1 2 3 8
+4 6 7 8
+-5 6 3 4
+
+Sample Output:
+-5 4 1
+6 6 2
+3 7 3
+4 8 8
+ */
 package two_dimensional_arrays;
-
 import java.util.Scanner;
-
 public class tda_8 {
     public static void main (String[]args){
         Scanner in=new Scanner(System.in);

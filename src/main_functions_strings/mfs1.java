f54533851b03b97a73b5db8f50deb71b3b184c23
vremyavnikuda <nevsky.andrew@gmail.com>
2022-11-30 17:47:31 +0700

    adding problem condition

--- src/main_functions_strings/mfs1.java
+++ src/main_functions_strings/mfs1.java
@@ -1,7 +1,28 @@
+/*
+Это цифра?
+Напишите программу, определяющую, является ли данный символ цифрой или нет.
+
+Входные данные
+Задан единственный символ c.
+
+Выходные данные
+Необходимо вывести  строку "yes", если символ является цифрой, и строку "no" в противном случае.
+
+Sample Input 1:
+8
+
+Sample Output 1:
+yes
+
+Sample Input 2:
+e
+
+Sample Output 2:
+no
+*/
+
 package main_functions_strings;
-
 import java.util.Scanner;
-
 public class mfs1 {
     public static void main (String[]args){
         Scanner sc=new Scanner(System.in);

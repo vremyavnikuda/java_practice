718d5978884092f4d494a52ea82b09de2b43c02e
vremyavnikuda <nevsky.andrew@gmail.com>
2022-12-02 22:33:34 +0700

    of_1 task "error compilation"

---
+++ src/offer_task/of_1.java
@@ -0,0 +1,11 @@
+package offer_task;
+
+public class of_1 {
+    void print(String str){
+        System.out.println(str);
+    }
+    public static void main (String[]args){
+        print("hello");
+    }
+}
+//ошибка компиляции

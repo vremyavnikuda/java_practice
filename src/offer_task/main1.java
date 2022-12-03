9d9c60638c18dd7f8eb8e7d69ccb18983cdd9995
vremyavnikuda <nevsky.andrew@gmail.com>
2022-12-04 05:00:12 +0700

    offer task

---
+++ src/offer_task/main1.java
@@ -0,0 +1,7 @@
+package offer_task;
+
+public class main1 {
+    public static void main (String[]args){
+        System.out.println(Planet.MARS.getRadius() + " " + Planet.EARTH.getRadius());
+    }
+}

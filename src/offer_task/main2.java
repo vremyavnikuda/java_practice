d2bbaf31738595199d67ea926f28e34ab9cfae6f
vremyavnikuda <nevsky.andrew@gmail.com>
2022-12-04 05:00:22 +0700

    offer task

---
+++ src/offer_task/main2.java
@@ -0,0 +1,9 @@
+package offer_task;
+
+import static offer_task.Formatter.JSON;
+
+public class main2 {
+    public static void main (String[]args){
+        System.out.println(JSON.getCount());
+    }
+}

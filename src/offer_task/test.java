7cdc3025af8e860dc63cfa86e0b8d1785c129d37
vremyavnikuda <nevsky.andrew@gmail.com>
2022-12-06 10:22:10 +0700

    task offer test

---
+++ src/offer_task/test.java
@@ -0,0 +1,16 @@
+/*
+package offer_task;
+import org.junit.Test;
+
+@Test
+public static class Comparator<@Test T>
+implements @Test Comparable<@Test T> {
+
+@Override
+public int compareTo(@Test T elem) {
+    @Test String doubled = elem + elem;
+    String @Test out = (@Test String) elem;
+    return 0;
+    }
+}
+/*

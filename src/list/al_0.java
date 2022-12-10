024bc8aa2e57805f056c5e10450d6b6b8077a312
vremayvnikuda <nevsky.andrew@gmail.com>
2022-12-11 01:32:40 +0700

    offer task(a tall order)

---
+++ src/list/al_0.java
@@ -0,0 +1,13 @@
+package list;
+import java.io.*;
+public class al_0 {
+    public static void main (String[]args){
+        try(BufferedReader sc=new BufferedReader(new InputStreamReader(System.in))){
+            int q= sc.read(),w= sc.read(),e= sc.read();
+
+        }
+        catch (IOException run) {
+            System.out.println(run.getMessage());
+        }
+    }
+}

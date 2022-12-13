7671c11d4a5a967e0a9042c0f3a85d9d99741ad6
vremayvnikuda <nevsky.andrew@gmail.com>
2022-12-14 02:48:59 +0700

    offer task

---
+++ src/offer_task/offertask_1.java
@@ -0,0 +1,28 @@
+/*package offer_task;
+
+import java.util.List;
+
+public class offertask_1 {
+    public class Parent {
+        public void print() {
+            System.out.println("Parent");
+        }
+    }
+
+    public class Child extends Parent {
+        @Override
+        public static void print() {
+            System.out.println("Child");
+        }
+    }
+
+    public static void print(List<Parent> list) {
+        list.forEach(Child::print);
+    }
+
+    public static void main(String[] args) {
+        List<Parent> parentList = List.of(new Child());
+        print(parentList);
+    }
+}
+*/

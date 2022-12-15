af7a984fddc10f2525291a4e3e3a573b45bca89f
vremayvnikuda <nevsky.andrew@gmail.com>
2022-12-16 03:00:40 +0700

    test function

---
+++ src/oop/test_1.java
@@ -0,0 +1,25 @@
+package oop;
+
+public class test_1 {
+    public static void main (String args[]){
+        int a = 6;
+        int b = 8;
+        sum(a, b);  // 14
+        sum(3, a);  // 9
+        sum(5, 23); // 28
+        display("Tom", 34);
+        display("Bob", 28);
+        display("Sam", 23);
+
+    }
+
+    static void sum(int x, int y){
+        int z = x + y;
+        System.out.println(z);
+
+    }
+    static void display(String name, int age){
+        System.out.println(name);
+        System.out.println(age);
+    }
+}

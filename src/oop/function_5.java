b41c24e313fde31afbe1be56a10e1f145ed6d88d
vremayvnikuda <nevsky.andrew@gmail.com>
2022-12-17 16:56:40 +0700

    task function"prime numbers on the interval"

---
+++ src/oop/function_5.java
@@ -0,0 +1,32 @@
+package oop;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class function_5 {
+    public static void main (String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        simple(a);
+        simple(b);
+        simple(c);
+    }
+
+    private static void simple(int a){
+        //флаг начального значения
+        boolean probablePrime=false;
+        //определяем является ли число простым методом Рабина-Миллера
+        BigInteger bigInteger = BigInteger.valueOf(a);
+        probablePrime = bigInteger.isProbablePrime((int) Math.log(a));
+        //так же добавляем исключения в условие a!=1&&a!=0&&a!=1
+        if (probablePrime==true&&a!=1&&a!=0&&a!=1){
+            //если число простое
+            System.out.println("YES");
+        }else {
+            //если нет 
+            System.out.println("NO");
+        }
+    }
+}

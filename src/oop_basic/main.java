f349ee836f7f2096ca5e1b354cfd5335dd8113d6
vremayvnikuda <nevsky.andrew@gmail.com>
2023-01-07 00:51:15 +0700

    конструктор с параметрами

--- src/oop_basic/main.java
+++ src/oop_basic/main.java
@@ -2,15 +2,15 @@ package oop_basic;
 
 public class main {
     public static void main(String[] args){
-        MyClass t1=new MyClass();
-        MyClass t2=new MyClass();
+        MyClass t1=new MyClass(10);
+        MyClass t2=new MyClass(20);
         System.out.println(t1.num+" "+t2.num);
     }
 
 }
 class MyClass{
     int num;
-    MyClass(){
-        num=100;
+    MyClass(int i){
+        num=i;
     }
 }

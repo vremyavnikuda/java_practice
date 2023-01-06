f0ebed3390de9256a5f5dac8de47fecb9efc3212
vremayvnikuda <nevsky.andrew@gmail.com>
2023-01-07 02:57:59 +0700

    +комментарии

--- src/oop_basic/main.java
+++ src/oop_basic/main.java
@@ -2,8 +2,11 @@ package oop_basic;
 
 public class main {
     public static void main(String[] args){
+        //изменяем параметры 1 объекта
         MyClass t1=new MyClass(10);
+        //измеряем параметры второго объекта
         MyClass t2=new MyClass(20);
+        //выводи результат в консоль
         System.out.println(t1.num+" "+t2.num);
     }
 
@@ -11,6 +14,7 @@ public class main {
 class MyClass{
     int num;
     MyClass(int i){
+
         num=i;
     }
 }

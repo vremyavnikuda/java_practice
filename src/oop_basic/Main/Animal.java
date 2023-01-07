c905643ef0ccb14a97dd750090bbf5882ee97857
vremayvnikuda <nevsky.andrew@gmail.com>
2023-01-08 02:56:06 +0700

    upd
    +комментарии
    +изменен конструктор

--- src/oop_basic/Main/Animal.java
+++ src/oop_basic/Main/Animal.java
@@ -1,4 +1,12 @@
 package oop_basic.Main;
+/*
+Животные 2
+Дополните класс Animal. У класса Animal должны быть следующие конструкторы:
+
+Позволяющий создать животное с его типом и именем
+Позволяющий создать животное с его типом и возрастом. При этом в имени окажется значение "No Name"
+Позволяющий создать животное со всеми его свойствами в том же порядке, который указан на предыдущем слайде.
+*/
 
 //определяем класс
 public class Animal {
@@ -11,11 +19,27 @@ public class Animal {
     boolean isFly;
     boolean isWalk;
     boolean isSwim;
+    //конструктор
+    //создать животное с его типом и именем
+    public Animal(String name,String type){
+        this.type=type;
+        this.name=name;
+        //System.out.println("--------------------");
+    }
+    //Конструктор
+    //Создать животное с его типом и возрастом.
+    // При этом в имени окажется значение "No Name"
+    public Animal(String type,int age){
+        this.type=type;
+        this.age=age;
+        this.name="No Name";
+        //System.out.println("--------------------");
+    }
 
     //Конструктор
+    //Позволяющий создать животное со всеми его свойствами в том же порядке, который указан на предыдущем слайде.
 
-    public Animal (/*String type,String name,int age,double weight,boolean isFly,
-                   boolean isWalk,boolean isSwim*/){
+    public Animal (String type,String name,int age,double weight,boolean isFly,boolean isWalk,boolean isSwim){
         this.type=type;
         this.name=name;
         this.age=age;
@@ -23,6 +47,7 @@ public class Animal {
         this.isFly=isFly;
         this.isWalk=isWalk;
         this.isSwim=isSwim;
+        //System.out.println("--------------------");
     }
 
     //Методы класса
@@ -31,9 +56,10 @@ public class Animal {
     }
     public void rename(String Rename){
         name=Rename;
-        System.out.println(name);
+        return;
     }
     public void holiday(int days){
-        System.out.println(weight+=days*0.1);
+        weight+=days*0.1;
+        return;
     }
 }
--- src/oop_basic/Main/Main.java
+++ src/oop_basic/Main/Main.java
@@ -2,28 +2,24 @@ package oop_basic.Main;
 
 class Main{
     public static void main(String[] args) {
-        Animal tiger = new Animal();
-        tiger.type = "тигр";
-        tiger.name = "Артём";
+        Animal tiger = new Animal("тигр", "Артём");
         tiger.age = 15;
         tiger.weight = 300.6;
         tiger.isSwim = true;
         tiger.isWalk = true;
         tiger.isFly = false;
         tiger.display();
-        tiger.rename("Антон");
-        tiger.display();
 
-        Animal sparrow = new Animal();
-        sparrow.type = "воробей";
-        sparrow.name = "Капитан Джек";
-        sparrow.age = 3;
+        Animal sparrow = new Animal("воробей", 5);
         sparrow.weight = 2;
         sparrow.isSwim = false;
         sparrow.isWalk = true;
         sparrow.isFly = true;
         sparrow.display();
-        sparrow.holiday(2);
-        sparrow.display();
+
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8, true, true, true);
+        duck.holiday(5);
+        duck.rename("Утя-Утя");
+        duck.display();
     }
 }

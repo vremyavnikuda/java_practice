6b438c7ef68b5bd4e203e47b6e9e4327db9b5d0f
vremayvnikuda <nevsky.andrew@gmail.com>
2023-01-15 04:25:36 +0700

    переопределение метода display()
    в класса Pen,Ruler,Divider метод display() отображает информацию к какому классу он относится

---
+++ src/oop_basic/Переопределённые_письменные_принадлежности/Pen.java
@@ -0,0 +1,39 @@
+package oop_basic.Переопределённые_письменные_принадлежности;
+
+public class Pen extends WritingMaterials {
+    private int countColor;
+    private boolean auto;
+
+    public Pen() {
+        super("ручка", "Красный", 145, 15.6, true);
+    }
+
+
+    //метод writeMyName, который выводит на экран строчку, содержащую имя
+    public void writeMyName(){
+        System.out.println("vremyavnikuda");
+    }
+
+    //countColor
+    public void setCountColor(int countColor) {
+        this.countColor=countColor;
+    }
+
+    public int getCountColor() {
+        return countColor;
+    }
+
+    //auto
+    public void setAuto(boolean auto) {
+        this.auto=auto;
+    }
+
+    public boolean isAuto() {
+        return auto;
+    }
+
+    //переопределение метода display()
+    public void display(){
+        System.out.println("This is"+getClass());
+    }
+}
---
+++ src/oop_basic/Переопределённые_письменные_принадлежности/Ruler.java
@@ -0,0 +1,39 @@
+package oop_basic.Переопределённые_письменные_принадлежности;
+
+public class Ruler extends WritingMaterials {
+    private double length;
+    private boolean wood;
+
+    public Ruler() {
+        super("ручка", "Красный", 145, 15.6, true);
+    }
+
+    //метод measure
+    public void measure(){
+        System.out.println("Сейчас померяем длину");
+    }
+
+    //Length
+    public void setLength(double length){
+        this.length=length;
+    }
+
+
+    public double getLength() {
+        return length;
+    }
+
+    //wood
+    public void setWood(boolean wood){
+        this.wood=wood;
+    }
+
+    public boolean isWood(){
+        return wood;
+    }
+
+    //переопределение метода display()
+    public void display(){
+        System.out.println("This is"+ getClass());
+    }
+}

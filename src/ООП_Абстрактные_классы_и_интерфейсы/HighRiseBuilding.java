4b363ab4955e03c8fd8dad153ff8424818b0575d
vremayvnikuda <nevsky.andrew@gmail.com>
2023-02-12 21:11:11 +0700

    upd HighRiseBuilding class
    add: 
    - boolean elevator -> если в здании лифт(true/false)
    - int numFloors -> количество этажей в HighRiseBuilding
    - String nameComplex -> название жилого комплекса (к которому относится данное здание)
    
    методы:
    - метод displayInfo() -> отображение информации о классе HighRiseBuilding -> переопределение метода
        класса Building displayInfo()
    - метод locatedBuilding() "В каком районе находится здание?"
    - метод tallBuilding() -> Это здание является высотным ?

--- src/ООП_Абстрактные_классы_и_интерфейсы/HighRiseBuilding.java
+++ src/ООП_Абстрактные_классы_и_интерфейсы/HighRiseBuilding.java
@@ -1,22 +1,31 @@
+/*
+добавить 3 метода
+так же можно перегружать методы
+*/
 package ООП_Абстрактные_классы_и_интерфейсы;
+
+import java.util.Scanner;
 
 //HighRiseBuilding
 public class HighRiseBuilding extends Building {
-    // 3 cвойства HighRiseBuilding
+
     //boolean elevator -> если в здании лифт(true/false)
     private boolean elevator;
+
     //int numFloors -> количество этажей в HighRiseBuilding
     private int numFloors;
+
     // String nameComplex -> название жилого комплекса (к которому относится данное здание)
     private String nameComplex;
 
+    //Конструктор класса HighRiseBuilding
     public HighRiseBuilding(
             String titleBuildeing,
             String addressBuildeing,
             int year_of_constructionBuildeing,
             String name_architectBuildeing,
             boolean cultural_monumentBuildeing
-    ){
+    ) {
         super(
                 titleBuildeing,
                 addressBuildeing,
@@ -28,31 +37,67 @@ public class HighRiseBuilding extends Building {
         this.numFloors = numFloors;
     }
 
+    //Getter/Setter
     //get/set int numFloors
-    public int getNumFloors(){
+    public int getNumFloors() {
         return numFloors;
     }
-    public void setNumFloors(int numFloors){
-        this.numFloors=numFloors;
+
+    public void setNumFloors(int numFloors) {
+        this.numFloors = numFloors;
     }
 
     //is/set boolean elevator
-    public boolean isElevator(){
+    public boolean isElevator() {
         return elevator;
     }
-    public void setElevator(boolean elevator){
-        this.elevator=elevator;
+
+    public void setElevator(boolean elevator) {
+        this.elevator = elevator;
     }
 
     //get/set String nameComplex
-    public String getNameComplex(){
+    public String getNameComplex() {
         return nameComplex;
     }
-    public void setNameComplex(String nameComplex){
-        this.nameComplex=nameComplex;
+
+    public String setNameComplex(String nameComplex) {
+        this.nameComplex = nameComplex;
+        return nameComplex;
     }
-    /*
-    добавить 3 метода
-    так же можно перегружать методы
-    */
+
+    //МЕТОДЫ
+
+    //метод displayInfo() -> отображение информации о классе HighRiseBuilding -> переопределение метода
+    //класса Building displayInfo()
+    @Override
+    public void displayInfo() {
+        System.out.println("Здание: " + getTitleBuildeing());
+        System.out.println("Адрес: " + getAddressBuildeing());
+        System.out.println("Год постройки: " + getYear_of_constructionBuildeing());
+        System.out.println("Имя архитектора: " + getName_architectBuildeing());
+        System.out.println("Культурное наследие: " + isCultural_monumentBuildeing());
+        System.out.println("Здание находится в: " + locatedBuilding());
+        System.out.println("Здание является высотным " + tallBuilding());
+    }
+    //метод обработки "В каком районе находится здание?"
+    @Override
+    public String locatedBuilding() {
+        Scanner input_locatedBuilding=new Scanner(System.in);
+        System.out.println("В каком районе находится здание?");
+        String located=input_locatedBuilding.nextLine();
+        input_locatedBuilding.close();
+        return located+" районе";
+    }
+
+    //метод tallBuilding() -> Это здание является высотным ?
+    @Override
+    public String tallBuilding() {
+        if (numFloors>10){
+            return "YES";
+        }else {
+            return "NO";
+        }
+    }
 }
+

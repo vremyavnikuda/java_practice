527df16d39a4ad6aeadf627f0fd355b3f5ef9600
vremayvnikuda <nevsky.andrew@gmail.com>
2023-02-15 19:31:20 +0700

    добавлены свойства класса
    numHouse -> кол-во комнат в House
    numberFloorsHouse -> кол-во этажей в доме?
    whetherPoolHouse -> Есть ли бассейн??
    garageHouse -> Есть ли гараж?

--- src/ООП_Абстрактные_классы_и_интерфейсы/House.java
+++ src/ООП_Абстрактные_классы_и_интерфейсы/House.java
@@ -4,6 +4,15 @@ package ООП_Абстрактные_классы_и_интерфейсы;
 public class House extends Building {
     //int numRooms -> кол-во комнат в House
     private int numRooms;
+
+    //кол-во этажей в доме?
+    private int numberFloorsHouse;
+
+    //Есть ли бассейн?
+    private boolean whetherPoolHouse;
+
+    //Есть ли гараж?
+    private boolean garageHouse;
     public House(
             String titleBuildeing,
             String addressBuildeing,
@@ -19,13 +28,44 @@ public class House extends Building {
                 cultural_monumentBuildeing
         );
     this.numRooms=numRooms;
+    this.numberFloorsHouse=numberFloorsHouse;
+    this.whetherPoolHouse=whetherPoolHouse;
+    this.garageHouse=garageHouse;
+
     }
-    //get/set numHouse
+
+    //Getter/Setter
+    //get/set numHouse -> кол-во комнат в House
     public int getNumHouse(){
         return numRooms;
     }
     public void setNumHouse(int numRooms){
         this.numRooms=numRooms;
     }
+
+    //get/set numberFloorsHouse -> кол-во этажей в доме?
+    public int getNumberFloorsHouse(){
+        return numberFloorsHouse;
+    }
+    public void setNumberFloorsHouse(int numberFloorsHouse){
+        this.numberFloorsHouse=numberFloorsHouse;
+    }
+
+    //get/set whetherPoolHouse -> Есть ли бассейн??
+    public boolean isWhetherPoolHouse(){
+        return whetherPoolHouse;
+    }
+    public void setWhetherPoolHouse(boolean whetherPoolHouse){
+        this.whetherPoolHouse=whetherPoolHouse;
+    }
+
+    //get/set garageHouse -> Есть ли гараж?
+    public boolean isGarageHouse(){
+        return garageHouse;
+    }
+    public void setGarageHouse(boolean garageHouse){
+        this.garageHouse=garageHouse;
+    }
+
 }
 

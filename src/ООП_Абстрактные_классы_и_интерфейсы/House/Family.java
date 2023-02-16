a5f8011c9c3a2074611e3b5bf9a4a5e09bfb205b
vremayvnikuda <nevsky.andrew@gmail.com>
2023-02-16 21:34:12 +0700

    upd class

--- src/ООП_Абстрактные_классы_и_интерфейсы/House/Family.java
+++ src/ООП_Абстрактные_классы_и_интерфейсы/House/Family.java
@@ -1,38 +1,32 @@
 package ООП_Абстрактные_классы_и_интерфейсы.House;
 
-public class Family extends House {
-    /*
 
-    -> Хватает ли комнат каждому члену семьи ?
-
-    -> Хватит ли места в гараже для автомобилей?
-    */
+// TODO: 16.02.2023 class Family наследует свойства и методы от class House
+public class Family{
 
     //numFamilyComposition -> Количество членов семьи
-    int numFamilyComposition;
+    static int numFamilyComposition;
+
     // String[]familyComposition -> Состав семьи
     public String[]familyComposition =new String[numFamilyComposition];
+
     // hasChildren -> Есть ли дети?
     boolean hasChildren;
+
     // numberFamilyHasChildren -> Количество детей в семье
     int numberFamilyHasChildren;
+
     //hasCarFamily -> Есть ли автомобиль у семьи?
     boolean hasCarFamily;
-    //numbCarFamily -> сколько автомобилей в семье ?
-    int numbCarFamily;
 
-    public Family(String titleBuildeing,
-                  String addressBuildeing,
-                  int year_of_constructionBuildeing,
-                  String name_architectBuildeing,
-                  boolean cultural_monumentBuildeing) {
-        super(
-                titleBuildeing,
-                addressBuildeing,
-                year_of_constructionBuildeing,
-                name_architectBuildeing,
-                cultural_monumentBuildeing
-        );
+    //numbCarFamily -> сколько автомобилей в семье ?
+    static int numbCarFamily;
+
+    public Family() {
+        this.numFamilyComposition=numFamilyComposition;
+
+
+
     }
     //МЕТОДЫ
 }

98b76bc39034860d843e1ae3e178f54d472c617f
vremyavnikuda <andrew.nevsky@yandex.ru>
2023-02-22 22:32:05 +0700

    основные типы данных add task1

---
+++ src/основы_java/Основные_типы_данных.Ввод_данных_с_клавиатуры/task1.java
@@ -0,0 +1,14 @@
+package основы_java.Основные_типы_данных.Ввод_данных_с_клавиатуры;
+
+/*Создайте две переменные: целого типа a и действительного b, равные 5 и 0.4 соответственно. Выведите построчно на экран их сумму, разность, произведение и частное.*/
+public class task1 {
+    public static void main(String []args){
+        int a=5;
+        double b = 0.4;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}

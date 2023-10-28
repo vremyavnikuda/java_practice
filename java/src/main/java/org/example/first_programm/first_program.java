823a1b537b0df43b07555ca99a1b3ad25fa1135a
andrew nevsky <andrew.nevsky@yandex.ru>
2023-10-28 10:44:17 +0700

    add folder >java and

---
+++ java/src/main/java/org/example/first_programm/first_program.java
@@ -0,0 +1,7 @@
+package org.example.first_programm;
+
+public class first_program {
+	public static void main (String []args){
+		System.out.println("Hello world!");
+	}
+}
---
+++ java/src/main/java/org/example/two_string/two_string.java
@@ -0,0 +1,8 @@
+package org.example.two_string;
+
+public class two_string {
+	public static void main (String []args){
+		System.out.println("Hello");
+		System.out.println("Programming!");
+	}
+}

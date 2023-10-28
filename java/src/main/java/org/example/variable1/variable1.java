7721ceb31e4d7444694638dd93ae163b51d72049
andrew nevsky <andrew.nevsky@yandex.ru>
2023-10-28 15:22:13 +0700

    добавлены решение задач java

--- java/src/main/java/org/example/variable1/variable1.java
+++ java/src/main/java/org/example/variable1/variable1.java
@@ -7,20 +7,58 @@ public class variable1 {
 		int a = 5;
 		double b = 0.4;
 
-		System.out.println(a+b);
-		System.out.println(a-b);
-		System.out.println(a*b);
-		System.out.println(a/b);
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		/*
 		scannerInt();
+		work_1();
+		work_2();
+		square_and_perimeter();
+		 */
+		nextEven();
+
 	}
 
 	private static void scannerInt() {
-		Scanner readerNum=new Scanner(System.in);
+		Scanner readerNum = new Scanner(System.in);
 		int a = readerNum.nextInt();
 		int b = readerNum.nextInt();
-		System.out.println(a+b);
+		System.out.println(a + b);
 	}
 
+	private static void work_1() {
+		Scanner readerNum = new Scanner(System.in);
+		int a = readerNum.nextInt();
+		int b = readerNum.nextInt();
+		int c = readerNum.nextInt();
+		System.out.println(a * b * c);
+	}
+
+	private static void work_2() {
+		Scanner readerNumber = new Scanner(System.in);
+		int a = readerNumber.nextInt();
+
+		System.out.println("The next number for the number " + a + " is " + (a + 1) + ".");
+		System.out.println("The previous number for the number " + a + " is " + (a - 1) + ".");
+	}
+
+	// Square and perimeter
+	private static void square_and_perimeter() {
+		Scanner readerNumbers = new Scanner(System.in);
+		int a = readerNumbers.nextInt();
+		int b = readerNumbers.nextInt();
+		System.out.println(a*b);
+		System.out.println((a+b)*2);
+	}
+
+	//Next even
+	private static void nextEven() {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		System.out.println(n+2-(n%2));
+	}
 }
 
 

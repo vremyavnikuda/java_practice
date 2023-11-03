e3584aee30adfa9704b9b9826a9e7a094b36d605
andrew nevsky <andrew.nevsky@yandex.ru>
2023-11-03 21:13:11 +0700

    conditional_operator_a
    Equation3
    Point1

--- java/src/main/java/org/example/Задачи_на_условный_оператор_Уровень_А/conditional_operator_A.java
+++ java/src/main/java/org/example/Задачи_на_условный_оператор_Уровень_А/conditional_operator_A.java
@@ -9,8 +9,10 @@ public class conditional_operator_A {
 		numberOfDigits();
 		numberOfDigits_1();
 		Equation1();
-		*/
 		Equation2();
+		Equation3();
+		*/
+		Point1();
 	}
 
 	private static void Temperature() {
@@ -78,14 +80,67 @@ public class conditional_operator_A {
 		int c = sc.nextInt();
 		sc.close();
 
-		int discriminnat=b*b-4*a*c;
+		int discriminnat = b * b - 4 * a * c;
 
-		if (discriminnat>0){
+		if (discriminnat > 0) {
 			System.out.println(2);
-		} else if (discriminnat==0) {
+		} else if (discriminnat == 0) {
 			System.out.println(1);
-		}else {
+		} else {
 			System.out.println(0);
 		}
 	}
+
+	//Equation 3
+	private static void Equation3() {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		sc.close();
+
+		if (a == 0) {
+			if (b == 0) {
+				if (c == 0) {
+					System.out.println("inf");
+				}else {
+					System.out.println(0);
+				}
+			} else {
+				System.out.println(1);
+			}
+		} else {
+
+			int dicriminant = b * b - 4 * a * c;
+
+			if (dicriminant > 0) {
+				System.out.println(2);
+			} else if (dicriminant == 0) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
+		}
+	}
+
+	//Point 1
+	private static void Point1(){
+		Scanner sc=new Scanner (System.in);
+
+		double x=sc.nextDouble();
+		double y= sc.nextDouble();
+
+		sc.close();
+
+		if (y > 2 * x){
+			System.out.println("Yes");
+		}else{
+			System.out.println("No");
+		}
+	}
 }
+
+
+
+
